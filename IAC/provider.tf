217ebd636d39a466f01b5f09e3b71cc2e2235569
Aravinth Nallasamy <nallasamya@science.regn.net>
2024-07-22 09:18:12 +0530

    fix: Changing the IAC

--- IAC/provider.tf
+++ IAC/provider.tf
@@ -8,15 +8,15 @@ terraform {
 }
 
 # Retrieve secret value from Key Vault
-#data "azurerm_key_vault_secret" "client_secret" {
-#  name         = "AISDLC"
-#  key_vault_id = "24b080cd-5874-44ab-9862-8d7e0e0781ab"
-#}
+data "azurerm_key_vault_secret" "client_secret" {
+  name         = "AISDLC"
+  key_vault_id = "24b080cd-5874-44ab-9862-8d7e0e0781ab"
+}
 
 provider "azurerm" {
   features {}
   subscription_id = "434d9ccf-0fbf-4203-a25a-314f7650f6f7"
   client_id       = "bf8bc755-1024-412f-b6c2-ee47258506fa"
-  client_secret   = "L4F8Q~zoAu5OrhFEtiqWCFuKUWIrvwgHjDDdrcZk"
+  client_secret   = data.azurerm_key_vault_secret.client_secret.value
   tenant_id       = "24b080cd-5874-44ab-9862-8d7e0e0781ab"
 }
