3bcde2fab6fed73b8f9e0f663f426d4190b45bfb
anjalibisht7 <50041006+anjalibisht7@users.noreply.github.com>
2024-08-12 16:17:03 +0530

    Update provider.tf

--- IAC/provider.tf
+++ IAC/provider.tf
@@ -15,15 +15,8 @@ data "azurerm_key_vault_secret" "client_secret" {
 
 provider "azurerm" {
   features {}
-<<<<<<< Updated upstream
-  subscription_id = "434d9ccf-0fbf-4203-a25a-314f7650f6f7"
-  client_id       = "bf8bc755-1024-412f-b6c2-ee47258506fa"
-  client_secret   = "L4F8Q~zoAu5OrhFEtiqWCFuKUWIrvwgHjDDdrcZk"
-  tenant_id       = "24b080cd-5874-44ab-9862-8d7e0e0781ab"
-=======
   subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
   tenant_id       = var.tenant_id
->>>>>>> Stashed changes
 }
