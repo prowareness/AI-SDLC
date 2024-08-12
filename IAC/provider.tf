263720cd4ba6ffdc52e5011e139c75a9c37d0b6e
anjalibisht7 <50041006+anjalibisht7@users.noreply.github.com>
2024-08-12 14:26:28 +0530

    Update provider.tf

--- IAC/provider.tf
+++ IAC/provider.tf
@@ -17,6 +17,6 @@ provider "azurerm" {
   features {}
   subscription_id = "434d9ccf-0fbf-4203-a25a-314f7650f6f7"
   client_id       = "bf8bc755-1024-412f-b6c2-ee47258506fa"
-  client_secret   = data.azurerm_key_vault_secret.client_secret.value
+  client_secret   = "L4F8Q~zoAu5OrhFEtiqWCFuKUWIrvwgHjDDdrcZk"
   tenant_id       = "24b080cd-5874-44ab-9862-8d7e0e0781ab"
 }
