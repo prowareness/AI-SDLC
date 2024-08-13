432467bfc0cc7716b5f6aac20755222e285ec43d
anjalibisht7 <50041006+anjalibisht7@users.noreply.github.com>
2024-08-13 14:01:55 +0530

    Update provider.tf

--- IAC/provider.tf
+++ IAC/provider.tf
@@ -7,16 +7,9 @@ terraform {
   }
 }
 
-# Retrieve secret value from Key Vault
-data "azurerm_key_vault_secret" "client_secret" {
-  name         = "AISDLC"
-  key_vault_id = "24b080cd-5874-44ab-9862-8d7e0e0781ab"
-}
+
 
 provider "azurerm" {
   features {}
-  subscription_id = var.subscription_id
-  client_id       = var.client_id
-  client_secret   = var.client_secret
-  tenant_id       = var.tenant_id
+  
 }
