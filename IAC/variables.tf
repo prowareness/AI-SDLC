fa1634691d776f61d3f4b84674bd78d0f90f0268
Shivanagouda Govindagoudar <68856301+shivanbgtsi@users.noreply.github.com>
2024-10-09 18:13:36 +0530

    commenting secrets

--- IAC/variables.tf
+++ IAC/variables.tf
@@ -1,19 +1,19 @@
-variable "subscription_id" {
-  description = "Azure Subscription ID"
-  default     = "434d9ccf-0fbf-4203-a25a-314f7650f6f7"
-}
+# variable "subscription_id" {
+#   description = "Azure Subscription ID"
+#   default     = "434d9ccf-0fbf-4203-a25a-314f7650f6f7"
+# }
 
-variable "client_id" {
-  description = "Azure Client ID"
-  default     = "bf8bc755-1024-412f-b6c2-ee47258506fa"
-}
+# variable "client_id" {
+#   description = "Azure Client ID"
+#   default     = "bf8bc755-1024-412f-b6c2-ee47258506fa"
+# }
 
-variable "client_secret" {
-  description = "Azure Client Secret"
-  default     = "L4F8Q~zoAu5OrhFEtiqWCFuKUWIrvwgHjDDdrcZk"
-}
+# variable "client_secret" {
+#   description = "Azure Client Secret"
+#   default     = "L4F8Q~zoAu5OrhFEtiqWCFuKUWIrvwgHjDDdrcZk"
+# }
 
-variable "tenant_id" {
-  description = "Azure Tenant ID"
-  default     = "24b080cd-5874-44ab-9862-8d7e0e0781ab"
-}
+# variable "tenant_id" {
+#   description = "Azure Tenant ID"
+#   default     = "24b080cd-5874-44ab-9862-8d7e0e0781ab"
+# }
